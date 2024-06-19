99b18907690f6c45aad4cc9bb63e03c15761a547
dwlhm <dwiilhamm026@gmail.com>
2024-06-19 10:51:12 +0700

    [frontend] make the logs view floating

--- web/src/routes/__auth/perangkat/$perangkatId.logs.lazy.tsx
+++ web/src/routes/__auth/perangkat/$perangkatId.logs.lazy.tsx
@@ -29,40 +29,42 @@ function DeviceLogs() {
   if (isError) return <Errors process="get log perangkat" message={error} />;
   if (isSuccess)
     return (
-      <div className="mb-10">
-        <div className="flex justify-between items-center">
-          <h3 className="text-lg font-medium">Log Perangkat</h3>
-          <Link
-            to="/perangkat/$perangkatId"
-            params={{ perangkatId }}
-            className="inline-block float-right"
-          >
-            <div className="bg-red-900 p-1 rounded w-full transition border border-2 border-red-900 transition hover:border-red-600">
-              <XMarkIcon className="block h-6 w-6 text-white" />
-            </div>
-          </Link>
+      <div className="my-10 fixed inset-0 z-30 flex items-center justify-center">
+        <div className="max-w-5xl overflow-y-auto bg-white rounded max-h-full">
+          <div className="flex justify-between items-center sticky inset-0 bg-white/50 p-5 backdrop-blur">
+            <h3 className="text-lg font-medium">Log Perangkat</h3>
+            <Link
+              to="/perangkat/$perangkatId"
+              params={{ perangkatId }}
+              className="inline-block float-right"
+            >
+              <div className="bg-red-900 p-1 rounded w-full transition border border-2 border-red-900 transition hover:border-red-600">
+                <XMarkIcon className="block h-6 w-6 text-white" />
+              </div>
+            </Link>
+          </div>
+          <ul className="p-5">
+            {data.results.map((item, index) => (
+              <li key={`logs.${perangkatId}.${index}`} className="mb-5">
+                <p className="flex items-center capitalize font-medium gap-2 mb-2">
+                  {new Date(item.created_at).toLocaleString()}:
+                  <span
+                    className={
+                      item.type == "error"
+                        ? "text-red-500"
+                        : item.type == "warning"
+                          ? "text-yellow-500"
+                          : "text-blue-500"
+                    }
+                  >
+                    {item.type}
+                  </span>
+                </p>
+                <p className="bg-gray-200 rounded p-2">{item.message}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="mt-5">
-          {data.results.map((item, index) => (
-            <li key={`logs.${perangkatId}.${index}`} className="mb-5">
-              <p className="flex items-center capitalize font-medium gap-2 mb-2">
-                {new Date(item.created_at).toLocaleString()}:
-                <span
-                  className={
-                    item.type == "error"
-                      ? "text-red-500"
-                      : item.type == "warning"
-                        ? "text-yellow-500"
-                        : "text-blue-500"
-                  }
-                >
-                  {item.type}
-                </span>
-              </p>
-              <p className="bg-gray-200 rounded p-2">{item.message}</p>
-            </li>
-          ))}
-        </ul>
       </div>
     );
 }
